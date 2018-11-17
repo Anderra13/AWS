a1bd358e22523e94d78b03fb9393cdb1091ffd71
Troffifi <raluca.trofin@stud.acs.upb.ro>
2018-11-17 18:27:03 +0200

    print all labels in ontology for drugs

--- AWSproiect9.py
+++ AWSproiect9.py
@@ -4,6 +4,11 @@ from owlready2 import *
 if __name__ == "__main__":
     onto = get_ontology("file://DINTO.owl").load()
 
-    annotations = onto.annotation_properties()
-    for ann in annotations:
-        print(ann)
+    with onto:
+        pharmacological_entity = onto.search_one(label="pharmacological entity")
+        print(pharmacological_entity.label)
+        drugs = list(pharmacological_entity.subclasses())
+        for drug in drugs:
+            print(drug.label)
+
+        annotations = onto.annotation_properties()
